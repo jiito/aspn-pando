785b62da1d6d38d49af0d77d81385ea362285c10
jiito <benjamin.allanrahill@gmail.com>
2022-11-30 13:24:57 -0500

    [asp-95]: find by id

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -19,7 +19,8 @@ async fn main() -> std::io::Result<()> {
             .service(
                 web::scope("/host")
                     .route("", web::post().to(routes::host::create))
-                    .route("/{hostId}", web::get().to(routes::host::find))
+                    .route("", web::get().to(routes::host::find))
+                    .route("/{hostId}", web::get().to(routes::host::find_by_id))
                     .route("/{hostId}/connect", web::post().to(routes::host::connect)),
             )
             .service(
--- src/routes/host.rs
+++ src/routes/host.rs
@@ -1,9 +1,19 @@
 use actix_web::{web, HttpResponse};
+use serde::{Deserialize, Serialize};
 
 pub async fn create() -> HttpResponse {
     todo!("implement the create host handler")
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FindQueryParams {
+    token: Option<String>,
+}
 pub async fn find(path: web::Path<i32>) -> HttpResponse {
+    let id = path.into_inner();
+    todo!("implement the find host handler")
+}
+pub async fn find_by_id(path: web::Path<i32>) -> HttpResponse {
     let id = path.into_inner();
     todo!("implement the find host handler")
 }
