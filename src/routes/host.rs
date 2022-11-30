1569ed71bed9ed5c3b428b208e563a30f2dc5cfe
jiito <benjamin.allanrahill@gmail.com>
2022-11-30 11:18:27 -0500

    [asp-95]: initial host crud handlers

---
+++ src/routes/host.rs
@@ -0,0 +1,8 @@
+use actix_web::HttpResponse;
+
+pub fn create() -> HttpResponse {
+    todo!("implement the create host handler")
+}
+pub fn find() -> HttpResponse {
+    todo!("implement the find host handler")
+}
