94d6958a22e7a97cb8908924b1aafe6bf4ab308b
jiito <benjamin.allanrahill@gmail.com>
2022-11-30 12:32:09 -0500

    [asp-95]: parse id

--- src/routes/host.rs
+++ src/routes/host.rs
@@ -4,5 +4,6 @@ pub async fn create() -> HttpResponse {
     todo!("implement the create host handler")
 }
 pub async fn find(path: web::Path<i32>) -> HttpResponse {
+    let id = path.into_inner();
     todo!("implement the find host handler")
 }
