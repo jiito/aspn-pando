2b39b403a4d14266eff45367d1372f88a7084ce6
jiito <benjamin.allanrahill@gmail.com>
2022-11-30 13:11:10 -0500

    [asp-95]: add error handling

--- src/routes/function.rs
+++ src/routes/function.rs
@@ -12,14 +12,20 @@ pub struct FindQueryParams {
 pub async fn find_by_id(path: web::Path<i32>) -> HttpResponse {
     let id = path.into_inner();
     let conn = &mut establish_connection();
-    let function = storage::db::functions::find_by_id(conn, &id).unwrap();
-    HttpResponse::Ok().json(function)
+    let function = storage::db::functions::find_by_id(conn, &id);
+    match function {
+        Ok(function) => HttpResponse::Ok().json(function),
+        Err(_) => HttpResponse::InternalServerError().into(),
+    }
 }
 pub async fn find(path: web::Query<FindQueryParams>) -> HttpResponse {
     let conn = &mut establish_connection();
     if let Some(id) = path.project_id {
-        let function = storage::db::functions::find_by_project(conn, &id).unwrap();
-        HttpResponse::Ok().json(function)
+        let function = storage::db::functions::find_by_project(conn, &id);
+        match function {
+            Ok(function) => HttpResponse::Ok().json(function),
+            Err(_) => HttpResponse::InternalServerError().into(),
+        }
     } else {
         HttpResponse::InternalServerError().into()
     }
