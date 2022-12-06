26d9d93520d1a95675c909e60177a7619ccea95c
jiito <benjamin.allanrahill@gmail.com>
2022-12-06 11:33:26 -0500

    [main]: use env config db

--- src/storage/db.rs
+++ src/storage/db.rs
@@ -6,11 +6,12 @@ pub mod project;
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenvy::dotenv;
-use std::env;
+
+use crate::config;
 
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let database_url = config::env::database_url();
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
